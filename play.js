cc5b48df4bc3c76c2311e6fbd40383993bf57d02
Colleen Garvey <colleenellengarvey@gmail.com>
2022-06-08 15:43:39 -0400

    created idle message

---
+++ play.js
@@ -0,0 +1,24 @@
+// THIS IS WHERE WE LAUNCH THE GAME CLIENT
+
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    // code that does something when the connection is first established
+    console.log(data);
+  });
+
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
