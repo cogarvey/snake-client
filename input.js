dd3a7eec48029d5dc8596c0c96f07cc4e039ee66
Colleen Garvey <colleenellengarvey@gmail.com>
2022-06-08 17:11:51 -0400

    created special keys

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
 // setup interface to handle user input from stdin
+// Stores the active TCP connection object.
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,8 +12,30 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function (key) {
-  if (key === '\u0003' || key === 'e') {
+const handleUserInput = function(key) {
+
+  if (key === '\u0003' || key === 'p') {
     process.exit();
   };
-};
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  if (key === 'm') {
+    connection.write('Say: Move please!');
+  }
+  if (key === 'n') {
+    connection.write("Say: NOM NOM NOM");
+  }
+}
+
+
+module.exports = { setupInput };
